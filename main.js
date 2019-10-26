23d0c4f2712155e619828547a15deae703630a48
Lorenzo Scaturchio <lorenzosca7@gmail.com>
2019-10-26 06:47:25 -0700

    Got the server side running, got basic implementation of Google Cloud Vision API in main.js

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
-const request = require("express");
-const routes = require("./src/routes");
+const express = require("express");
+// const routes = require("./src/routes");
+const path = require("path");
 
-// All Api routes
-app.use(
-    "/api",
-    routes.attendees,
-);
+const app = express();
 
 // Render React Application
 app.use(express.static(path.join(__dirname, "/client/build/")));
+
+app.listen(3000, () => {
+    console.log("listening on 3000");
+});
+
+// Bring forth Google cloud Vision API
+async function quickstart() {
+    // Imports the Google Cloud client library
+    const vision = require('@google-cloud/vision');
+    // Creates a client
+    const client = new vision.ImageAnnotatorClient();  
+    // Performs label detection on the image file
+    const [result] = await client.labelDetection('./resources/wakeupcat.jpg');
+    const labels = result.labelAnnotations;
+    console.log('Labels:');
+    labels.forEach(label => console.log(label.description));
+}
