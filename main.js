0b90a0bc9f32d3c0fa89a4bac3afc035234d8d2f
Lorenzo Scaturchio <lorenzosca7@gmail.com>
2019-10-26 08:49:43 -0700

    got the localhost to run

--- main.js
+++ main.js
@@ -6,32 +6,33 @@ const app = express();
 
 // Render React Application
 app.use(express.static(path.join(__dirname, "/client/build/")));
-
-app.listen(3000, () => {
-    console.log("listening on 3000");
+app.get('/', function(req, res) {
+    res.send("listening on 3000")
 });
 
-// Imports the Google Cloud client libraries
-const vision = require('@google-cloud/vision');
-const fs = require('fs');
+app.listen(3000);
 
-// Creates a client
-const client = new vision.ImageAnnotatorClient();
+// // Imports the Google Cloud client libraries
+// const vision = require('@google-cloud/vision');
+// const fs = require('fs');
+
+// // Creates a client
+// const client = new vision.ImageAnnotatorClient();
 
 /**
  * TODO(developer): Uncomment the following line before running the sample.
  */
 
 // const fileName = `/./`;
-const request = {
-  image: {content: fs.readFileSync(fileName)},
-};
+// const request = {
+//   image: {content: fs.readFileSync(fileName)},
+// };
 
-const [result] = await client.objectLocalization(request);
-const objects = result.localizedObjectAnnotations;
-objects.forEach(object => {
-  console.log(`Name: ${object.name}`);
-  console.log(`Confidence: ${object.score}`);
-  const vertices = object.boundingPoly.normalizedVertices;
-  vertices.forEach(v => console.log(`x: ${v.x}, y:${v.y}`));
-});
+// const [result] = await client.objectLocalization(request);
+// const objects = result.localizedObjectAnnotations;
+// objects.forEach(object => {
+//   console.log(`Name: ${object.name}`);
+//   console.log(`Confidence: ${object.score}`);
+//   const vertices = object.boundingPoly.normalizedVertices;
+//   vertices.forEach(v => console.log(`x: ${v.x}, y:${v.y}`));
+// });
