9a91eba19722a099b0efbb404c6d09f9fa60a93c
APstarwars <32665017+APstarwars@users.noreply.github.com>
2019-10-27 16:12:04 -0700

    Update UI
    Make the UI a little cleaner.

--- rwebsite/src/index.js
+++ rwebsite/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const elementary = <h1>Insert an image here</h1>
+const elementary = <h1>Ingredion</h1>
 
 ReactDOM.render(elementary, document.getElementById('root'));
