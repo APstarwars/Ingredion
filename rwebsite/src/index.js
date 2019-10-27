cb97a03a88cdad62f5e282c0a8d4e1d365a0ce2f
APstarwars <32665017+APstarwars@users.noreply.github.com>
2019-10-27 16:45:53 -0700

    Add Image Upload Functionality
    Users can now upload files

--- rwebsite/src/index.js
+++ rwebsite/src/index.js
@@ -1,6 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const elementary = <h1>Ingredion</h1>
+// Credit to Purgatory on Stackoverflow for the code snippet
+class FileInput extends React.Component {
+    constructor(props) {
+      super(props)
+      this.uploadFile = this.uploadFile.bind(this);
+    }
+    
+    uploadFile(event) {
+        let file = event.target.files[0];
+        console.log(file);
+        
+        if (file) {
+          let data = new FormData();
+          data.append('file', file);
+        }
+    }
+    
+    render() {
+      return <span>
+        <input type="file"
+        name="myFile"
+        onChange={this.uploadFile} />
+      </span>
+    }
+}
 
-ReactDOM.render(elementary, document.getElementById('root'));
+ReactDOM.render(<FileInput />, document.getElementById('root'));
