317b735d3791fe6d874d79b18d70cced437c0b72
APstarwars <32665017+APstarwars@users.noreply.github.com>
2019-10-28 00:54:18 -0700

    Try to get external html to work
    Didn't work

--- client.js
+++ client.js
@@ -6,22 +6,21 @@ http.createServer(function (req, res) {
   if (req.url == '/fileupload') {
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
-      var oldpath = files.filetoupload.path;
-      var newpath = 'C:/Users/hobbe/Documents/Github/Cal-Hacks-6.0/assets/img/' + files.filetoupload.name;
-      fs.rename(oldpath, newpath, function (err) {
-        if (err) throw err;
-        res.write('File uploaded and moved!');
-        res.end();
-      });
+        var oldpath = files.filetoupload.path;
+        var newpath = 'C:/Users/hobbe/Documents/Github/Cal-Hacks-6.0/assets/img/' + files.filetoupload.name;
+        fs.rename(oldpath, newpath, function (err) {
+            if (err) throw err;
+            res.write('File uploaded and moved!');
+            res.end();
+        });
     });
   } else {
-    fs.readFile("./client/public/index.html", function(err, data) {
         res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('<h1>Insert an image here</h1>');
         res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
         res.write('<input type="file" name="filetoupload"><br>');
         res.write('<input type="submit">');
         res.write('</form>');
         return res.end();
-    });
   }
 }).listen(8080); 
